1128df5745aa45fa6813816b10d71b1adb60c184
v1enna <19vienna20@gmail.com>
2021-11-29 12:37:26 +0100

    Improve error handling on SignIn and Login

--- src/main/java/Control/LoginServlet.java
+++ src/main/java/Control/LoginServlet.java
@@ -4,14 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import Model.User;
 import Service.UserService;
 import Utils.PasswordHasher;
@@ -25,18 +23,45 @@ public class LoginServlet extends HttpServlet {
 		HttpServletRequest request,
 		HttpServletResponse response
 	) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+	
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		if(username == null || password == null) {
+			String failureMessage = "";
+
+			/*
+				TC_Login
+				-> TC_LoginFailed4
+			*/
+			failureMessage += (username == null) ? "Username: campo obbligatorio.\n" : "";
+
+			/*
+				TC_Login
+				-> TC_LoginFailed2
+			*/
+			failureMessage += (password == null) ? "Password: campo obbligatorio.\n" : "";
+
+			out.print(failureMessage);
+			System.out.println("# LoginServlet > Tentativo di login fallito (value(s) == null).");
+			response.setStatus(400);
+			return;
+		}
+
 		Connection db = (Connection) request.getServletContext().getAttribute("databaseConnection");
 		UserService service = new UserService(db);
-		PrintWriter out = response.getWriter();
-		
-		int id = service.getIdByUsername(request.getParameter("username"));
+		int id = service.getIdByUsername(username);
 			
 		if(id != -1) {
 			User user = service.getUser(id); 
-				
-			if(user.getPassword().equals(PasswordHasher.hash((request.getParameter("password"))))) {
-				response.setStatus(200);
-					
+
+			if(user.getPassword().equals(PasswordHasher.hash((password)))) {
+				/*
+					TC_Login
+					-> TC_LoginOK
+				*/
+				response.setStatus(200);	
 				response.addCookie(new Cookie("user_session", request.getSession().getId()));
 				request.getSession().setAttribute("user_metadata", user);
 					
@@ -53,11 +78,15 @@ public class LoginServlet extends HttpServlet {
 				if(request.getParameter("cookie").equals("false"))
 					out.print(response.encodeURL(""));
 				
-				System.out.println("# LoginServlet > Login effettuato con successo da " + user.getName());
+				System.out.println("# LoginServlet > Login effettuato!");
 				System.out.println("# LoginServlet > Tentativo di creazione del cookie: [user_session = " + request.getSession().getId() + "]");
 				
 				return;
 			} else {
+				/*
+					TC_Login
+					-> TC_LoginFailed1
+				*/
 				out.print("La password &egrave; errata!");
 				System.out.println("# LoginServlet > Tentativo di login fallito (password errata).");
 					
@@ -65,6 +94,10 @@ public class LoginServlet extends HttpServlet {
 				return;
 			}
 		} else {
+			/*
+				TC_Login
+				-> TC_LoginFailed3
+			*/
 			out.print("L'username &egrave; errato o inesistente!");
 			System.out.println("# LoginServlet > Tentativo di login fallito (username errato o inesistente).");
 				
--- src/main/java/Control/SignInServlet.java
+++ src/main/java/Control/SignInServlet.java
@@ -3,7 +3,6 @@ package Control;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,44 +20,138 @@ public class SignInServlet extends HttpServlet {
 		HttpServletResponse response
 	) throws ServletException, IOException {
 		Connection db = (Connection) request.getServletContext().getAttribute("databaseConnection");
-		
-		UserService service = new UserService(db);
 		PrintWriter out = response.getWriter();
-		
-		int id = service.getIdByUsername(request.getParameter("username"));
-		boolean emailUsed = service.findUserByEmail(request.getParameter("email"));
-		
-		if(id == -1){
-			if(!emailUsed) {
-				if(request.getParameter("password").equals(request.getParameter("password2"))) {
-					service.insertUser(request.getParameter("username"), PasswordHasher.hash(request.getParameter("password")), request.getParameter("email"));
-					
-					out.print("Utente creato con successo!");
-					System.out.println("# SignInServlet > Creazione utente effettuata con successo (username: " + request.getParameter("username"));
-					
-					response.setStatus(200);
-					return;
-				} else {
-					out.print("Le password non coincidono!");
-					System.out.println("# SignInServlet > E' stato effettuato un tentativo fallimentare di registrazione.");
-					
-					response.setStatus(400);
-					return;
-				}
-			} else {
-				out.print("Questa email &egrave; gi&agrave; in uso!!");
-				System.out.println("# SignInServlet > E' stato effettuato un tentativo di creazione di un utente gia' esistente {err: email}.");
-			
-				response.setStatus(400);
-				return;	
-			}
-		} else {
-			out.print("Questo nome utente &egrave; gi&agrave; in uso!");
-			System.out.println("# SignInServlet > E' stato effettuato un tentativo di creazione di un utente gia' esistente {err: username}.");
-			
+
+		String failureMessage = "";
+
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String repeatPassword = request.getParameter("password2");
+		String email = request.getParameter("email");
+
+		boolean usernamePattern = username.matches("^[A-Za-z][A-Za-z0-9_]{7,29}$");
+		boolean passwordPattern = password.matches("/^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{5,}$/");
+		boolean repeatPasswordPattern = repeatPassword.matches("/^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{5,}$/");
+		boolean emailPattern = email.matches("^\\S+@\\S+$");
+
+		/*
+		 	Error Handling
+		*/
+
+		// Uno o più dati sono nulli
+		boolean nullable = username == null || password == null || repeatPassword == null || email == null;
+
+		// Uno o più dati non rispettano il formato richiesto
+		boolean mismatch = !usernamePattern || !passwordPattern || !repeatPasswordPattern || !emailPattern;
+
+		if(nullable || mismatch) {
+			/*
+				TC_Login
+				-> TC_RegistrazioneFailed12
+			*/
+			failureMessage += (username == null) ? "Username - campo obbligatorio.\n" : "";
+
+			/*
+				TC_Login
+				-> TC_RegistrazioneFailed8
+			*/
+			failureMessage += (password == null) ? "Password - campo obbligatorio.\n" : "";
+
+			/*
+				TC_Login
+				-> TC_RegistrazioneFailed6
+			*/
+			failureMessage += (repeatPassword == null) ? "Ripeti password - campo obbligatorio.\n" : "";
+
+			/*
+				TC_Login
+				-> TC_RegistrazioneFailed3
+			*/
+			failureMessage += (email == null) ? "Email - campo obbligatorio.\n" : "";
+
+			/*
+				TC_Login
+				-> TC_RegistrazioneFailed11
+			*/
+			failureMessage += (email != null && !usernamePattern) ? "Formato username non valido.\n" : "";
+
+			/*
+				TC_Login
+				-> TC_RegistrazioneFailed7
+			*/
+			failureMessage += (password != null && !passwordPattern) ? "Formato password non valido.\n" : "";
+
+			/*
+				TC_Login
+				-> TC_RegistrazioneFailed5
+			*/
+			failureMessage += (repeatPassword != null && !repeatPasswordPattern) ? "Formato ripeti password non valido.\n" : "";
+
+			/*
+				TC_Login
+				-> TC_RegistrazioneFailed2
+			*/
+			failureMessage += (email != null && !emailPattern) ? "Formato email non valido.\n" : "";
+
+			out.print(failureMessage);
+			System.out.println("# LoginServlet > Tentativo di login fallito: alcuni dati non risultano validi.");
+			System.out.println("# LoginServlet > Errore/i: " + failureMessage);
+
 			response.setStatus(400);
 			return;
+		} else {
+			UserService service = new UserService(db);
+
+			int id = service.getIdByUsername(username);
+			boolean emailUsed = service.findUserByEmail(email);
+				
+			if(id == -1) {
+				if(!emailUsed) {
+					if(password.equals(repeatPassword)) {
+						/*
+							TC_Registrazione
+							-> TC_RegistrazioneOK
+						*/
+						service.insertUser(request.getParameter("username"), PasswordHasher.hash(password), email);
+						
+						out.print("Registrazione avvenuta con successo!");						
+						System.out.println("# SignInServlet > Creazione utente effettuata con successo (username: " + request.getParameter("username"));
+						
+						response.setStatus(200);
+						return;
+					} else {
+						/*
+							TC_Registrazione
+							-> TC_RegistrazioneFailed4
+						*/
+						out.print("Le password non coincidono.");
+						System.out.println("# SignInServlet > E' stato effettuato un tentativo fallimentare di registrazione.");
+						
+						response.setStatus(400);
+						return;
+					}
+				} else {
+					/*
+						TC_Registrazione
+						-> TC_RegistrazioneFailed1
+					*/
+					out.print("E-mail gi&agrave; esistente.");
+					System.out.println("# SignInServlet > E' stato effettuato un tentativo di creazione di un utente gia' esistente {err: email}.");
+				
+					response.setStatus(400);
+					return;	
+				}
+			} else {
+				/*
+					TC_Registrazione
+					-> TC_RegistrazioneFailed10
+				*/
+				out.print("Username gi&agrave; esistente.");
+				System.out.println("# SignInServlet > E' stato effettuato un tentativo di creazione di un utente gia' esistente {err: username}.");
+					
+				response.setStatus(400);
+				return;
+			}
 		}
 	}
-
 }
